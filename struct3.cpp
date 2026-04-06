925630c09982efbe48004c6d420c8467c5c8ea51
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-06 11:19:54 +0700

    membuat struct dan deklarasi variabel struct

---
+++ struct3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct alamatDetail
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
+int main()
+{
+    // deklarasi variable struct
+    mahasiswa mhs;
+
+
+}
+
