22e8fbd7ee6c5d0c364cbc17cd3b44abd54c2dfa
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-06 10:58:22 +0700

    membuat struct detail alamat dan mahasiswa

---
+++ struct2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct alamatDetail
+{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa
+{
+    string nama;
+    alamatDetail alamat;
+    int umur;
+};
+
