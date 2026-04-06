e78a079562ea8872f689762dffff6c74d03ac844
haifaamalinawahdan <haifanew3@gmail.com>
2026-04-06 10:42:38 +0700

    mengisi dan menampilkan data dengan loop

--- app.cpp
+++ app.cpp
@@ -33,5 +33,16 @@ int main()
     cout << "data keempat = " << datamu[3] << endl;
     cout << "data kelima = " << datamu[4] << endl;
 
-    
+    //mengisi data melalui perulangan
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Isikan data ke-" << (i + 1) << " : ";
+        cin >> datamu[i];
+    }
+
+    // menampilkan data dengan perulangan
+    for (int i = 0; i<= 4; i++)
+    {
+        cout << "Data ke-" << (i + 1)<< " : " <<datamu[i] << endl;
+    }
 }
